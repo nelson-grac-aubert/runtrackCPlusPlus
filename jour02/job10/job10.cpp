e4e11c28785b9c16b01211147dba06e6caeb3a1d
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-05-12 13:35:17 +0200

    feat : complete exercice 10

---
+++ jour02/job10/job10.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <limits> // Import for cin error management 
+#include <cmath> // Import for square root 
+using namespace std; 
+
+float getUserInput(string description) {
+
+    float input;
+    bool inputIsValid = false;
+
+    while (!inputIsValid) {
+        cout << description << endl;
+        cout << "Enter your number : ";
+        cin >> input; 
+
+        if (cin.fail()) {
+            cout << "Invalid input! An integer is expected.\n";
+            cin.clear(); // Removes the errors in cin
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore every character in the cin until you end up on an \n
+        }
+        else if (input < 0) {
+            cout << "Invalid input! Number must be positive.\n";
+        }
+        else {
+            inputIsValid = true; 
+        }
+    }
+
+    return input;
+}
+
+double square (float toBeRooted) {
+    return sqrt(toBeRooted); 
+}
+
+int main() {
+
+    int userInput = getUserInput("Enter the positive number you want rooted"); 
+    cout << "The square root of " << userInput << " is " << square(userInput); 
+
+    return 1;
+}
