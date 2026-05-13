a247159e6f5f409b44903bfba41527d5a862dbd6
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-05-13 10:02:31 +0200

    feat : complete job 02

---
+++ jour03/job02/job02.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isAVowel(char ch) {
+    if (ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 || ch == 121) {
+        return true;
+    }
+    return false;
+}
+
+string deleteVowels(string aString) {
+
+    for (int i = 0; i < aString.length(); i++) {
+
+        if (isAVowel(aString[i])) {
+            aString.erase(i, 1);
+        }
+    }
+
+    cout << aString << endl;
+    return aString;
+}
+
+int main() {
+
+    deleteVowels("vive la Plateforme!");
+    
+}
